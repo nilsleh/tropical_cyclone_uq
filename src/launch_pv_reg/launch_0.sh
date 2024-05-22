5331e087f00a485133a079b09336b5022e3bbec9
nm19000 <nina-maria.gottschling@dlr.de>
2024-05-22 12:00:14 +0200

    pv experiments

---
+++ src/launch_pv_reg/launch_0.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+CUDA_VISIBLE_DEVICES=0
+python run_cli_script.py model_config=configs/pv_reg/mc_dropout.yaml data_config=configs/pv_reg/dataset.yaml trainer_config=configs/pv_reg/trainer.yaml experiment.seed=10 trainer.devices=[0]
---
+++ src/launch_pv_reg/launch_3.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+CUDA_VISIBLE_DEVICES=3
+python run_cli_script.py model_config=configs/pv_reg/due.yaml data_config=configs/pv_reg/dataset.yaml trainer_config=configs/pv_reg/trainer.yaml experiment.seed=2 trainer.devices=[0]
