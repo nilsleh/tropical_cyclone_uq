3598a9f2fdae653c49bbc608c9c75ea82f9ed27b
Nils Lehmann <nils.lehmann24@gmail.com>
2024-05-24 13:13:18 +0200

    working model

--- src/skippd_model.py
+++ src/skippd_model.py
@@ -1,81 +1,72 @@
+from typing import Tuple
 import torch
-import torch.nn.functional as F
+from torch import nn
 
-#TODO: what are in_ find n_hidden/output in SKIPPD notebook, check if model translated correctly
-
-#import pdb
-
-#pdb.set_trace()
-
-class SkippdModel(torch.nn.Module):
+class SkippdModel(nn.Module):
     def __init__(self, 
-        input_dim: int = 24,  
-        num_classes: int=1,      
-        num_filters: int = 24,
-        kernel_size: int = 3,
-        pool_size: int=2,
-        strides: int = 2,
-        dense_size: int = 1024,
-        drop_rate: float = 0.4
-        ):
+            in_chans: int = 48,
+            num_filters: int = 24, 
+            kernel_size: int = 3, 
+            pool_size: int = 2, 
+            strides: int = 2, 
+            dense_size: int = 1024, 
+            drop_rate: float = 0.4) -> None:
+        
+        """Initialize a new instance of Skippd Model.
+        
+        Args:
+            in_chans: int, number of input channels
+            num_filters: int, number of filters in the first convolutional layer
+            kernel_size: int, size of the convolutional kernel
+            pool_size: int, size of the pooling kernel
+            strides: int, stride of the pooling kernel
+            dense_size: int, size of the dense layer
+            drop_rate: float, dropout rate
+        """
         super(SkippdModel, self).__init__()
 
-        self.kernel_size = kernel_size
-        self.strides = strides
-        self.num_filters = num_filters
-        self.pool_size = pool_size
-        self.dense_size = dense_size
-        self.drop_rate = drop_rate 
-        self.input_dim = input_dim
-        self.num_classes = num_classes
-
-
-        #check dimensions out/in_channels
-
-        self.layer1 = torch.nn.Sequential(
-            torch.nn.Conv2d(in_channels=self.input_dim, out_channels=self.input_dim, kernel_size=self.kernel_size),
-            torch.nn.ReLU(),
-            torch.nn.MaxPool2d(kernel_size = self.pool_size, stride=self.strides)
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_chans, num_filters, kernel_size, padding='same'),
+            nn.BatchNorm2d(num_filters),
+            nn.ReLU(),
+            nn.MaxPool2d(pool_size, strides)
         )
 
-        #check dimensions out/in_channels
-
-        self.layer2 = torch.nn.Sequential(
-            torch.nn.Conv2d(in_channels=self.input_dim, out_channels=2*self.input_dim, kernel_size=self.kernel_size),
-            torch.nn.BatchNorm2d(num_features = int(96)),
-            torch.nn.MaxPool2d(kernel_size = self.pool_size, stride=self.strides)
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(num_filters, num_filters*2, kernel_size, padding='same'),
+            nn.BatchNorm2d(num_filters*2),
+            nn.ReLU(),
+            nn.MaxPool2d(pool_size, strides)
         )
 
-        self.layer3 = torch.nn.Sequential(
-                torch.nn.Linear(in_features=int(12304), out_features=int(1024)),
-                torch.nn.ReLU()
+        self.flatten = nn.Flatten()
+
+        # Calculate the output size of the Flatten layer
+        conv_out_size: int = num_filters*2 * (64 // (pool_size*strides))**2  # assuming the input size is (64, 64)
+
+        self.fc = nn.Sequential(
+            nn.Linear(conv_out_size, dense_size),
+            nn.ReLU(),
+            nn.Dropout(drop_rate),
+            nn.Linear(dense_size, dense_size),
+            nn.ReLU(),
+            nn.Dropout(drop_rate),
+            nn.Linear(dense_size, 1)
         )
 
-        self.layer4 = torch.nn.Sequential(
-                torch.nn.Linear(in_features=int(1024), out_features=int(1024)),
-                torch.nn.ReLU()
-        )
+    def forward(self, x_in: torch.Tensor) -> torch.Tensor:
+        """Forward pass.
         
-        self.dropout = torch.nn.Dropout(p = self.drop_rate)
-
-        self.flatten = torch.nn.Flatten()
-        self.linear = torch.nn.Linear(in_features=int(1024), out_features=self.num_classes)
-        
-
-    def forward(self, x):
+        Args:
+            x_in: torch.Tensor, shape [batch_size, in_chans, 64, 64]
         """
-        x: Tensor, shape [64,64,3*16,8] - pixels, RGB channels*time steps, PV output of 8 time steps (?)
-        
-        """
-        x_2 = x[-1]
-        x = self.layer1(x)
-        x = self.layer2(x)
+        x: torch.Tensor = self.conv1(x_in)
+        x = self.conv2(x)
         x = self.flatten(x)
-        x = torch.cat((x,x_2),axis=1)
-        x = self.dropout(self.layer3(x))
-        x = self.dropout(self.layer4(x))
-        x = self.linear(x)
+        return self.fc(x)
 
-        return x
+# x = torch.randn(1, 48, 64, 64)
+# model = SkippdModel()
+# out = model(x)
 
-
+# import pdb
